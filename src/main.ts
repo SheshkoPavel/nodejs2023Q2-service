dbc0747f9fd2ccbfb23e999667a36302deee5681
Pavel Sheshko <psheshko@twelvedevs.com>
2023-07-27 17:34:33 +0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
+import { readFile } from 'fs/promises';
+import { join } from 'path';
+import { parse } from 'yaml';
+
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const document = await readFile(join(__dirname, '..', 'doc/api.yaml'), 'utf-8');
+  SwaggerModule.setup('docs', app, parse(document));
+
   await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
