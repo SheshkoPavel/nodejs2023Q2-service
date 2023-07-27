678e85cf0216368bd100f6356aa918e8224e0630
Pavel Sheshko <psheshko@twelvedevs.com>
2023-07-27 14:14:26 +0300

    feat: create inMemoryDB module for data base

---
+++ src/db/db.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { InMemoryDb } from './inMemoryDB';
+
+@Module({
+    providers: [InMemoryDb],
+    exports: [InMemoryDb],
+})
+export class DbModule { }
