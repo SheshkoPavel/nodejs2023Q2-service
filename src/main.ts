6df3bee4034d799f9724aec911f6e77b498bbd69
Pavel Sheshko <psheshko@twelvedevs.com>
2023-08-01 09:19:13 +0300

    feat: init commit

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import 'dotenv/config';
 
+//start work on task
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
