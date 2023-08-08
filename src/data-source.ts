04938306892657e814b73c35eb38d840fe1ad2c0
Pavel Sheshko <psheshko@twelvedevs.com>
2023-08-08 12:10:34 +0300

    feat: add migrations

--- src/data-source.ts
+++ src/data-source.ts
@@ -14,7 +14,7 @@ export const AppDataSource = new DataSource({
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
-  synchronize: true,
+  synchronize: false,
   logging: false,
   entities: [User, Artist, Album, Track, Favorites],
   subscribers: [],
