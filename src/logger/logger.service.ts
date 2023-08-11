72348bf2d718fff45811189b0a16a1f8c55f3497
Pavel Sheshko <psheshko@twelvedevs.com>
2023-08-11 16:04:05 +0300

    feat: add some more methods to logger for feature implementation

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -7,6 +7,31 @@ export class LoggingService extends ConsoleLogger {
   }
 
   // log(message: any, ...optionalParams: any[]) {
-  //   // console.log(message);
+  //   console.log(message);
+  // }
+
+  // error(message: any, ...optionalParams: any[]) {
+  //   console.log(message);
+  // }
+
+  // /**
+  //  * Write a 'warn' level log.
+  //  */
+  // warn(message: any, ...optionalParams: any[]) {
+  //   console.log(message);
+  // }
+
+  // /**
+  //  * Write a 'debug' level log.
+  //  */
+  // debug(message: any, ...optionalParams: any[]) {
+  //   console.log(message);
+  // }
+
+  // /**
+  //  * Write a 'verbose' level log.
+  //  */
+  // verbose(message: any, ...optionalParams: any[]) {
+  //   console.log(message);
   // }
 }
